7e2c4f9a0b31932f19bc4783f51c9c89096a9e36
Gordon Freeman <toxa82@gmail.com>
2015-08-21 23:09:47 +0300

    Init commit

---
+++ app/src/main/java/ua/dp/altermann/calc/SplashActivity.java
@@ -0,0 +1,34 @@
+package ua.dp.altermann.calc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TextView tvSplashName = (TextView) findViewById(R.id.tvSplashName);
+        Animation animN = AnimationUtils.loadAnimation(this, R.anim.splash);
+        tvSplashName.startAnimation(animN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
